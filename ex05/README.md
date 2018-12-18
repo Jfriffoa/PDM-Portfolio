067a5ec2727a5bf74a2d3d33b65cae188869e471
José Francisco Riffo <jfriffoa@gmail.com>
2018-12-18 18:49:10 -0300

    Ex05: Tic Tac Toe simple

---
+++ ex05/README.md
@@ -0,0 +1,6 @@
+# Ex05 - Tic Tac Toe
+
+Tic Tac Toe con interfaz gráfica simple.
+Se juega de dos jugadores, lleva un contador de partidas y un botón de reinicio para poder seguir jugando.
+
+[*Volver al indice*](../README.md)
--- ex05/app/src/main/java/cl/pdm/pencho/tictactoe/MainActivity.kt
+++ ex05/app/src/main/java/cl/pdm/pencho/tictactoe/MainActivity.kt
@@ -44,7 +44,7 @@ class MainActivity : AppCompatActivity() {
         val btns = arrayOf(b1, b2, b3, b4, b5, b6, b7, b8, b9)
         for (b in btns) {
             b.text = ""
-            b.setBackgroundColor(Color.DKGRAY)
+            b.setBackgroundResource(R.drawable.abc_btn_default_mtrl_shape)
             b.isClickable = true
         }
 
